0f730563d2ec0f60f253543e6f72fd807ad46578
Hipst0man <yaroslavmasyna@gmail.com>
2021-07-15 14:16:12 +0300

    Added logger class

---
+++ CPPLogger/CPPLogger.cpp
@@ -0,0 +1,69 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include "pch.h"
+#include "framework.h"
+
+#include "CPPLogger.h"
+
+const wchar_t* CLIENT_INIT_PARAM = L"/P7.Sink=FileTxt /P7.Dir=C:\Program Files\Logs\ /P7.Format=\"%ti - %tf [%lv] - [%fs] [%fn] %ms\"";
+const wchar_t* TRACE_CHANNEL = L"Trace";
+const bool TRACE_ID = NULL;
+const IP7_Trace::hModule I_HMODULE = NULL;
+
+Logger::Logger()
+{
+	m_log_level = eP7Trace_Level::EP7TRACE_LEVEL_TRACE;
+	m_client = P7_Create_Client(CLIENT_INIT_PARAM);
+	m_trace = P7_Create_Trace(m_client, TRACE_CHANNEL);
+}
+
+Logger::~Logger()
+{
+	m_trace->Release();
+	m_client->Release();
+}
+
+Logger& Logger::operator()(const std::string& func, const std::string& filename, short level)
+{
+	m_func_name = func;
+	m_file_name = filename;
+	m_log_level = (eP7Trace_Level)level;
+	return *this;
+}
+
+Logger& Logger::operator<<(const char* log_message)
+{
+	switch (m_log_level)
+	{
+	case eP7Trace_Level::EP7TRACE_LEVEL_DEBUG:
+		m_trace->Trace(TRACE_ID, eP7Trace_Level::EP7TRACE_LEVEL_DEBUG, NULL, (tUINT16)__LINE__, m_file_name.c_str(), m_func_name.c_str(), L"%s", log_message);
+		return *this;
+
+	case eP7Trace_Level::EP7TRACE_LEVEL_INFO:
+		m_trace->Trace(TRACE_ID, eP7Trace_Level::EP7TRACE_LEVEL_INFO, NULL, (tUINT16)__LINE__, m_file_name.c_str(), m_func_name.c_str(), L"%s", log_message);
+		return *this;
+
+	case eP7Trace_Level::EP7TRACE_LEVEL_WARNING:
+		m_trace->Trace(TRACE_ID, eP7Trace_Level::EP7TRACE_LEVEL_WARNING, NULL, (tUINT16)__LINE__, m_file_name.c_str(), m_func_name.c_str(), L"%s", log_message);
+		return *this;
+
+	case eP7Trace_Level::EP7TRACE_LEVEL_ERROR:
+		m_trace->Trace(TRACE_ID, eP7Trace_Level::EP7TRACE_LEVEL_ERROR, NULL, (tUINT16)__LINE__, m_file_name.c_str(), m_func_name.c_str(), L"%s", log_message);
+		return *this;
+
+	case eP7Trace_Level::EP7TRACE_LEVEL_CRITICAL:
+		m_trace->Trace(TRACE_ID, eP7Trace_Level::EP7TRACE_LEVEL_CRITICAL, NULL, (tUINT16)__LINE__, m_file_name.c_str(), m_func_name.c_str(), L"%s", log_message);
+		return *this;
+
+	default:
+		m_trace->Trace(TRACE_ID, eP7Trace_Level::EP7TRACE_LEVEL_TRACE, NULL, (tUINT16)__LINE__, m_file_name.c_str(), m_func_name.c_str(), L"%s", log_message);
+		return *this;
+	}
+}
+
+void Logger::set_filter_level(LogLevels level)
+{
+	m_trace->Set_Verbosity(I_HMODULE, (eP7Trace_Level)level);
+}
---
+++ CPPLogger/CPPLogger.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "P7_Client.h"
+#include "P7_Trace.h"
+
+
+enum LogLevels
+{
+	L_TRACE = 0,
+	L_DEBUG,
+	L_INFO,
+	L_WARNING,
+	L_ERROR,
+	L_CRITICAL,
+};
+
+class ILogger
+{
+public:
+	virtual ILogger& operator() (const std::string& func_name, const std::string& file_name, short level) = 0;
+	virtual ILogger& operator<< (const char* log_message) = 0;
+	virtual void set_filter_level(LogLevels level) = 0;
+};
+
+class Logger : public ILogger
+{
+public:
+	Logger();
+	~Logger();
+
+	Logger& operator()(const std::string& func, const std::string& filename, short level) override;
+	Logger& operator<<(const char* log_message) override;
+
+	void set_filter_level(LogLevels level) override;
+
+private:
+	eP7Trace_Level  m_log_level;
+	IP7_Client*     m_client;
+	IP7_Trace*      m_trace;
+	std::string     m_func_name;
+	std::string     m_file_name;
+};
+
+#define LOG_TRACE LOG(__FUNCTION__, __FILE__, eP7Trace_Level::EP7TRACE_LEVEL_TRACE)
+#define LOG_DEBUG LOG(__FUNCTION__, __FILE__, eP7Trace_Level::EP7TRACE_LEVEL_DEBUG)
+#define LOG_INFO LOG(__FUNCTION__, __FILE__, eP7Trace_Level::EP7TRACE_LEVEL_INFO)
+#define LOG_WARN LOG(__FUNCTION__, __FILE__, eP7Trace_Level::EP7TRACE_LEVEL_WARNING)
+#define LOG_ERROR LOG(__FUNCTION__, __FILE__, eP7Trace_Level::EP7TRACE_LEVEL_ERROR)
+#define LOG_FATAL LOG(__FUNCTION__, __FILE__, eP7Trace_Level::EP7TRACE_LEVEL_CRITICAL)
