f1ea74d3e085a5d6e078fc6effe3a8f276ba92dc
Kostiantyn Yehorov <tripolko60@gmail.com>
2021-07-21 14:17:55 +0300

    some fixes

---
+++ SMTPServer/SMTPServer.cpp
@@ -0,0 +1,106 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include "SMTPServer.h"
+
+void SMTPServer::AcceptConnections()
+{
+	SOCKET client_socket;
+
+	while (true)
+	{
+		SOCKADDR client_info;
+		int client_info_len = sizeof(client_info);
+
+		std::cout << "Waiting for incoming connection...\n";
+
+		if (INVALID_SOCKET == (client_socket = accept(m_server_socket, &client_info, &client_info_len)))
+		{
+			std::cout << "Error with client socket accepting!\n";
+		}
+
+		else
+		{
+			std::cout << "Accepted new connection. Now creating session thread...\n";
+
+			m_thread_pool->AddTask(WorkWithClient, client_socket);
+		}
+	}
+}
+
+void SMTPServer::WorkWithClient(SOCKET client_socket)
+{
+	MailSession mail_session(client_socket);
+	char buf[128];
+	ZeroMemory(&buf, sizeof(buf));
+
+	mail_session.SendResponse(WELCOME);
+
+	while (int len = recv(mail_session.get_socket(), (char*)&buf, sizeof(buf), 0))
+	{
+		if (221 == mail_session.Processes(buf))
+		{
+			std::cout << "End of work\n";
+			closesocket(mail_session.get_socket());
+			break;
+		}
+
+		ZeroMemory(&buf, sizeof(buf));
+	}
+}
+
+bool SMTPServer::Initialize()
+{
+	WSADATA wsa_data;
+
+	if (auto res = WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
+	{
+		std::cout << "Error with winsock initializing!\n";
+		return false;
+	}
+
+	return true;
+}
+
+bool SMTPServer::SetSocketSettings()
+{
+	XMLParser xml_parser;
+	SOCKADDR_IN server_info;
+
+	m_server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	m_thread_pool = std::make_unique<ThreadPool>(xml_parser.GetMaxWorkingThreads());
+
+	if (m_server_socket == INVALID_SOCKET)
+	{
+		std::cout << "Error with socket initializing!\n";
+		return false;
+	}
+
+	ZeroMemory(&server_info, sizeof(server_info));
+
+	server_info.sin_family = AF_INET;
+	server_info.sin_port = htons(xml_parser.GetListenerPort());
+	server_info.sin_addr.S_un.S_addr = inet_addr(xml_parser.GetIpAddress().c_str());
+
+	if (bind(m_server_socket, (sockaddr*)&server_info, sizeof(server_info)) == SOCKET_ERROR)
+	{
+		std::cout << "Error with socket binding!\n" << WSAGetLastError();
+		return false;
+	}
+
+	return true;
+}
+
+void SMTPServer::ServerStart()
+{
+	if (listen(m_server_socket, SOMAXCONN) == SOCKET_ERROR)
+	{
+		std::cout << "Error with server starting!\n";
+		exit(WSAGetLastError());
+	}
+
+	std::cout << "Server started!\n\n";
+
+	AcceptConnections();
+}
---
+++ SMTPServer/SMTPServer.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include <memory>
+#include <thread>
+#include <string>
+
+#include "ThreadPool.h"
+#include "MailSession.h"
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#include "..\XMLParser\XMLParser.h"
+
+constexpr auto SMTP_PORT = 25;
+constexpr auto WELCOME = 220;
+
+class SMTPServer
+{
+public:
+	SMTPServer() : m_server_socket(INVALID_SOCKET) {}
+
+	bool Initialize();
+	bool SetSocketSettings();
+	void ServerStart();
+
+private:
+	void AcceptConnections();
+	static void WorkWithClient(SOCKET client_socket);
+
+private:
+	SOCKET m_server_socket;
+	std::unique_ptr<ThreadPool> m_thread_pool;
+};
